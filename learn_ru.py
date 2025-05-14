730e396676dbd7a8e323b0f69abee61bbf018cc7
Valera <valerylarionov06@gmail.com>
2025-05-14 21:56:07 +0300

    6

--- learn_ru.py
+++ learn_ru.py
@@ -3,13 +3,25 @@ import cv2
 from doctr.models import crnn_vgg16_bn
 
 
-def recognize_text(img_path, device, model):
-    """картинка с одним словом, по y 40 размером приблизительно"""
+def load_image(img_path, device):
     # Чтение изображения
     img = cv2.imread(img_path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Преобразуем в RGB
+
+    height = 32
+    (h, w) = img.shape[:2]
+    ratio = w / h
+    width = int(ratio * height)
+    img = cv2.resize(img, (width, height))
+
     img_tensor = torch.from_numpy(img).permute(2, 0, 1).float().unsqueeze(0) / 255.
     img_tensor = img_tensor.to(device)
+    return img_tensor
+
+
+def recognize_text(img_path, device, model, num):
+    # получение изображения
+    img_tensor = load_image(img_path, device)
 
     # Распознавание
     with torch.no_grad():
@@ -17,8 +29,16 @@ def recognize_text(img_path, device, model):
 
     # Вывод результатов
     words, _ = zip(*out["preds"])
-    print(f"Распознанный текст: {words[0]}")
+    print(f"Распознанный текст({num}): {words[0]}")
     return words[0]
+
+
+def load_model(device, text, model_path):
+    model = crnn_vgg16_bn(pretrained=False, vocab=text)
+    model.load_state_dict(torch.load(model_path, map_location=device))
+    model = model.to(device)
+    model.eval()
+    return model
 
 
 def main():
@@ -26,15 +46,20 @@ def main():
         text = file.readline()
 
     # Путь к модели
-    model_path = r"D:\projects\doctr\crnn_vgg16_bn_20250513-212912.pt"
-
-    # Загрузка модели
+    model_path = r"my_models/crnn_vgg16_bn_20250514-201731.pt"
+    model_path2 = r"my_models/crnn_vgg16_bn_20250513-212912.pt"
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = crnn_vgg16_bn(pretrained=False, vocab=text)
-    model.load_state_dict(torch.load(model_path, map_location=device))
-    model = model.to(device)
-    model.eval()
-    recognize_text(r"C:\Users\valera\Desktop\rc7851.png", device, model)
+
+    # Загрузка моделей
+    model = load_model(device, text, model_path)
+    model2 = load_model(device, text, model_path2)
+
+    # путь к картинке
+    p = r"images/aa.jpg"
+
+    # распознавание
+    recognize_text(p, device, model, 1)
+    recognize_text(p, device, model2, 2)
 
 
 if __name__ == "__main__":
