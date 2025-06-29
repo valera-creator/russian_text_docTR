282a566d2ae0edf480f1c4037970d6bc18c93f56
Valera <valerylarionov06@gmail.com>
2025-06-30 00:29:32 +0300

    g

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import shutil
 
+# путь к ответам из старой разбивки train изображений
 with open(r'D:\projects\convert\train_path\labels.json', encoding='utf-8') as file:
     data = json.load(file)
 
@@ -18,12 +19,16 @@ for key in k:
             new_train[key] = val
         else:
             new_val[key] = val
+            # путь к каждой картинке из старой train разбивки
             src = r'D:\projects\convert\train_path\images' + "\\" + key
+            # путь к папке валидационных картинок
             dst = r'D:\projects\convert\val\images'
             shutil.move(src, dst)
 
+# путь к ответам картинок из train части
 with open(r'D:\projects\convert\train_path\train.json', encoding='utf-8', mode='w') as file:
     json.dump(new_train, file, indent=3, ensure_ascii=False)
 
+# путь к ответам картинок из валидационной части
 with open(r'D:\projects\convert\val\labels.json', encoding='utf-8', mode='w') as file:
     json.dump(new_val, file, indent=3, ensure_ascii=False)
